a029875b37760ffce905683647ac17a290b2af4f
Ion Pascari <Ion.Pascari@orange.com>
2023-02-26 22:22:22 +0200

    feat(lab): implement getAccountWithCustomer via async API Composition

--- api-gateway-service/src/main/java/net/chrisrichardson/bankingexample/apigateway/apigateway/AccountHandlers.java
+++ api-gateway-service/src/main/java/net/chrisrichardson/bankingexample/apigateway/apigateway/AccountHandlers.java
@@ -4,7 +4,6 @@ import net.chrisrichardson.bankingexample.apigateway.apigateway.proxies.AccountS
 import net.chrisrichardson.bankingexample.apigateway.apigateway.proxies.CustomerServiceProxy;
 import io.eventuate.examples.tram.sagas.ordersandcustomers.customers.apigateway.AccountWithCustomer;
 import net.chrisrichardson.bankingexample.accountservice.common.GetAccountResponse;
-import net.chrisrichardson.bankingexample.customerservice.common.CustomerInfo;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -12,24 +11,36 @@ import reactor.core.publisher.Mono;
 
 import javax.validation.constraints.NotNull;
 
-import java.util.Optional;
-
 import static org.springframework.web.reactive.function.BodyInserters.fromValue;
 
 public class AccountHandlers {
 
-  private AccountServiceProxy orderService;
-  private CustomerServiceProxy customerService;
+    private AccountServiceProxy orderService;
+    private CustomerServiceProxy customerService;
 
-  public AccountHandlers(AccountServiceProxy orderService, CustomerServiceProxy customerService) {
-    this.orderService = orderService;
-    this.customerService = customerService;
-  }
+    public AccountHandlers(AccountServiceProxy orderService, CustomerServiceProxy customerService) {
+        this.orderService = orderService;
+        this.customerService = customerService;
+    }
 
-  @NotNull
-  public Mono<ServerResponse> getAccountWithCustomer(ServerRequest serverRequest) {
-    throw new RuntimeException("not yet implemented");
-  }
+    @NotNull
+    public Mono<ServerResponse> getAccountWithCustomer(ServerRequest serverRequest) {
+        String accountId = serverRequest.pathVariable("accountId");
 
+        return orderService.findAccountById(accountId)
+                .flatMap(maybeAccount -> maybeAccount.map(account -> getAccountWithCustomerMono(account)
+                                .flatMap(AccountHandlers::serverResponse)).orElseGet(() -> ServerResponse.notFound().build()));
+
+    }
+
+    private static Mono<ServerResponse> serverResponse(AccountWithCustomer accountWithCustomer) {
+        return ServerResponse.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(fromValue(accountWithCustomer));
+    }
+
+    private Mono<AccountWithCustomer> getAccountWithCustomerMono(GetAccountResponse account) {
+        return customerService.findCustomerById(account.getAccountInfo().getCustomerId()).map(customer -> new AccountWithCustomer(account, customer));
+    }
 
 }
